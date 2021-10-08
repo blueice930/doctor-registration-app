5552de2ed9a945ba9fa09ac9a3504e8d6be7dea4
Ronnie <icatchaflyinmyhand@gmail.com>
2021-10-09 00:25:41 +0800

    ✨Init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CalendarView from './components/registrations/CalendarView';
+
+function App() {
+  return (
+    <div className="App">
+      <CalendarView />
+    </div>
+  );
+}
+
+export default App;
